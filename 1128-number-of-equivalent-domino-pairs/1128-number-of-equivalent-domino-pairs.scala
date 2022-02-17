35edd43687378c8a3e4f395e0b24ce7408697f65
Samu <9478529+scristobal@users.noreply.github.com>
2022-02-17 23:36:26 +0100

    Time: 6112 ms (50.00%), Space: 725.6 MB (50.00%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.scala
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.scala
@@ -1,9 +1,12 @@
+import scala.annotation.tailrec
+
 object Solution {
 
   def dominoEquivalent(domino: Array[Int], otherDomino: Array[Int]): Boolean =
     (domino(0) == otherDomino(0) && domino(1) == otherDomino(1)) ||
       (domino(0) == otherDomino(1) && domino(1) == otherDomino(0))
 
+  @tailrec
   def numEquivDominoPairsTail(dominoes: Array[Array[Int]], acc: Int): Int = {
 
     if (dominoes.length == 1) return acc
