a44bab425bc132a8408f217445e95679ed3d1734
Samu <9478529+scristobal@users.noreply.github.com>
2022-03-27 08:46:26 +0200

    Time: 561 ms (9.73%), Space: 6.9 MB (57.20%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.go
+++ 279-perfect-squares/279-perfect-squares.go
@@ -1,53 +1,24 @@
-func squares(n int) []int {
-
-	res := make([]int, n)
-
-	j := 0
-	i := float64(1)
-
-	for i <= float64(n) {
-		res[j] = int(i)
-		j += 1
-		i = (math.Pow(float64(j+1), 2))
-	}
-
-	inv := make([]int, j)
-
-	for i := j - 1; i >= 0; i-- {
-		inv[j-i-1] = res[i]
-	}
-
-	return inv
-}
-
-func numSquaresRecur(n int, sq []int, l int, b int) int {
-
-	for _, s := range sq {
-
-		if s == n {
-			return 1
-		}
-	}
-
-	m := b
-	for _, s := range sq {
-		if n-s > 0 && l+1 <= m {
-			r := numSquaresRecur(n-s, sq, l+1, m) + 1
-			if r < m {
-				m = r
-			}
-		}
-	}
-
-	return m
-
-}
-
 func numSquares(n int) int {
 
-	sq := squares(n)
+	numSquares := make(map[int]int, n+1)
 
-	fmt.Println(sq)
+	for i := 1; i <= n; i++ {
 
-	return numSquaresRecur(n, sq, 0, n)
+		min := i
+
+		for j := 1; i-j*j >= 0; j++ {
+			min = minInt(min, numSquares[i-j*j]+1)
+		}
+
+		numSquares[i] = min
+	}
+
+	return numSquares[n]
+}
+
+func minInt(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
