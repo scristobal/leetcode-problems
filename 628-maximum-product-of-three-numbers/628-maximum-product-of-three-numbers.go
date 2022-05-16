862374430eeeb44a16750471d6902a73e8d09570
Samu <9478529+scristobal@users.noreply.github.com>
2022-05-16 16:44:26 +0200

    Time: 35 ms (88.00%), Space: 6.6 MB (75.20%) - LeetHub

---
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.go
@@ -0,0 +1,40 @@
+func maximumProduct(nums []int) int {
+    var maxs = [3]int{math.MinInt, math.MinInt, math.MinInt}
+    var mins = [2]int{math.MaxInt, math.MaxInt}
+    
+    for _, v := range nums {
+        switch {
+            case v > maxs[2]: 
+                maxs[0] = maxs[1]
+                maxs[1] = maxs[2]
+                maxs[2] = v
+            case v > maxs[1]:
+                maxs[0] = maxs[1]
+                maxs[1] = v
+            case v > maxs[0]:
+                maxs[0] = v
+                
+        }
+        
+        switch {
+            case v < mins[1]:
+                mins[0] = mins[1]
+                mins[1] = v
+            case v < mins[0]:
+                mins[0] = v
+        }
+    }
+    
+  
+    fmt.Print("---")
+    fmt.Print(mins, maxs)
+    
+    min := mins[0]*mins[1]*maxs[2]
+    max := maxs[0]*maxs[1]*maxs[2]
+    
+    if min > max  {
+        return min
+    }
+    
+    return max
+}
