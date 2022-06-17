1251454ef35f8f256e7d2a58fa1b718b95cfa50b
Samu <9478529+scristobal@users.noreply.github.com>
2022-06-17 08:43:48 +0200

    Time: 124 ms (7.27%), Space: 8.1 MB (60.00%) - LeetHub

---
+++ 1476-subrectangle-queries/1476-subrectangle-queries.go
@@ -0,0 +1,35 @@
+type SubrectangleQueries struct {
+    values [][]int
+   
+}
+
+
+func Constructor(rectangle [][]int) SubrectangleQueries {
+    
+    values := rectangle
+   
+    
+    return SubrectangleQueries{values}
+}
+
+
+func (this *SubrectangleQueries) UpdateSubrectangle(row1 int, col1 int, row2 int, col2 int, newValue int)  {
+    for r := row1; r<=row2; r++ {
+        for c := col1; c <=col2; c++ {
+            this.values[r][c] = newValue
+        }
+    }
+}
+
+
+func (this *SubrectangleQueries) GetValue(row int, col int) int {
+    return this.values[row][col]
+}
+
+
+/**
+ * Your SubrectangleQueries object will be instantiated and called as such:
+ * obj := Constructor(rectangle);
+ * obj.UpdateSubrectangle(row1,col1,row2,col2,newValue);
+ * param_2 := obj.GetValue(row,col);
+ */
