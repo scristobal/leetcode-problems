9cc1a4da32f5546d03a7d7fa52e361549e51e544
Samu <9478529+scristobal@users.noreply.github.com>
2022-06-19 21:44:19 +0200

    Time: 282 ms (81.63%), Space: 14.8 MB (36.74%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.go
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.go
@@ -2,24 +2,20 @@ func rearrangeArray(nums []int) []int {
    
     res := make([]int, len(nums))
     
-    p, n, l:= 0, 0, -1
+    p, n:= 0, 1
     
-    for p < len(nums) && nums[p] < 0 { p++ }
-    for n<len(nums) && nums[n] > 0 { n++ }
     
     for j:=0; j < len(nums); j++ {
         
-        if l < 0 { 
-            res[j] = nums[p]
-            l = nums[p]
-            p++
-            for p < len(nums) && nums[p] < 0 { p++ }
-        } else {
-            res[j] = nums[n]
-            l = nums[n]
-            n++
-            for n<len(nums) && nums[n] > 0 { n++ }
+        if nums[j] > 0 {
+            res[p] = nums[j]
+            p +=2
+            continue
         }
+        
+        res[n] = nums[j]
+        n +=2
+        
     }
     
     return res
