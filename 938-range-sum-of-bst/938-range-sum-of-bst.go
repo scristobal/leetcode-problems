ba61e23cb672e4ddfec00d418cc44cc10685eb4b
Samu <9478529+scristobal@users.noreply.github.com>
2022-06-22 09:31:43 +0200

    Time: 123 ms (39.86%), Space: 9.8 MB (6.16%) - LeetHub

---
+++ 938-range-sum-of-bst/938-range-sum-of-bst.go
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, low int, high int) int {
+    
+    if root == nil { return 0 }
+    
+    acc := 0
+    
+    if low <= root.Val && root.Val <= high {
+        acc = root.Val
+    }
+    
+    acc += rangeSumBST(root.Left, low, high)
+    
+    acc += rangeSumBST(root.Right, low, high)
+    
+    return acc
+}
