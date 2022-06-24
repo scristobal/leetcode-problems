d5295e99daf9205910e2048e463318d4f5cb7cda
Samu <9478529+scristobal@users.noreply.github.com>
2022-06-24 19:29:10 +0200

    Time: 5 ms (83.93%), Space: 6.5 MB (8.93%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.go
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.go
@@ -2,20 +2,37 @@ func minOperations(boxes string) []int {
     
     l := len(boxes)
     
-    left := make([]int, l)
+    leftCh := make(chan []int)
     
-    for i, acc := 1, 0; i < l ; i++ {
-        if boxes[i-1] == byte(49) { acc++ }
-        left[i] = left[i-1] + acc
-    }
+    go func() {
+        
+        left := make([]int, l)
+    
+        for i, acc := 1, 0; i < l ; i++ {
+            if boxes[i-1] == byte(49) { acc++ }
+            left[i] = left[i-1] + acc
+        }
+        
+        leftCh <- left
+        
+    }()
     
     
-    right := make([]int, l)
+    rightCh := make(chan []int)
+    
+    go func(){
+        right := make([]int, l)
 
-    for i, acc := l-2, 0; i>=0; i-- {
-        if boxes[i+1] == byte(49) { acc++ }
-        right[i] = right[i+1] + acc
-    }
+        for i, acc := l-2, 0; i>=0; i-- {
+            if boxes[i+1] == byte(49) { acc++ }
+            right[i] = right[i+1] + acc
+        }
+        
+        rightCh <- right
+    }()
+    
+    left := <- leftCh
+    right := <- rightCh
     
     res := make([]int, l)
     
