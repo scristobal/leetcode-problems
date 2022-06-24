6ffa2b1de8d4e736267d36214cc8444090f80bae
Samu <9478529+scristobal@users.noreply.github.com>
2022-06-24 20:04:22 +0200

    Time: 100 ms (60.60%), Space: 8.5 MB (88.85%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.go
+++ 938-range-sum-of-bst/938-range-sum-of-bst.go
@@ -7,6 +7,9 @@
  * }
  */
 
+
+
+
 func rangeSumBST(root *TreeNode, low int, high int) int {
     
     if root == nil { return 0 }
@@ -17,53 +20,12 @@ func rangeSumBST(root *TreeNode, low int, high int) int {
         acc = root.Val
     }
     
-    left := make(chan int,1)
-    
-    func () {
-    
-        if low <= root.Val {
-            left <- rec(root.Left, low, high)
-        } else {
-            left <- 0
-        }
-        
-    }()
-    
-    right := make(chan int,1)
-    
-    func (){
-        
-        if root.Val <= high {
-            right <- rec(root.Right, low, high)
-        } else { 
-            right <- 0
-        }
-        
-    }()
-    
-    l := <- left
-    r := <- right
-    
-    return acc + r + l
-}
-
-
-func rec(root *TreeNode, low int, high int) int {
-    
-    if root == nil { return 0 }
-    
-    acc := 0
-    
-    if low <= root.Val && root.Val <= high {
-        acc = root.Val
-    }
-    
     if low <= root.Val {
-        acc += rec(root.Left, low, high)
+        acc += rangeSumBST(root.Left, low, high)
     }
     
     if root.Val <= high {
-        acc += rec(root.Right, low, high)
+        acc += rangeSumBST(root.Right, low, high)
     }
     
     return acc
