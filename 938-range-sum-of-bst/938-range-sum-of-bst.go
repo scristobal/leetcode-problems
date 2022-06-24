780e04cb9b11f9396a33f425b1dbf4a994789469
Samu <9478529+scristobal@users.noreply.github.com>
2022-06-24 20:03:21 +0200

    Time: 144 ms (28.25%), Space: 9.4 MB (36.06%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.go
+++ 938-range-sum-of-bst/938-range-sum-of-bst.go
@@ -17,15 +17,54 @@ func rangeSumBST(root *TreeNode, low int, high int) int {
         acc = root.Val
     }
     
+    left := make(chan int,1)
+    
+    func () {
+    
+        if low <= root.Val {
+            left <- rec(root.Left, low, high)
+        } else {
+            left <- 0
+        }
+        
+    }()
+    
+    right := make(chan int,1)
+    
+    func (){
+        
+        if root.Val <= high {
+            right <- rec(root.Right, low, high)
+        } else { 
+            right <- 0
+        }
+        
+    }()
+    
+    l := <- left
+    r := <- right
+    
+    return acc + r + l
+}
+
+
+func rec(root *TreeNode, low int, high int) int {
+    
+    if root == nil { return 0 }
+    
+    acc := 0
+    
+    if low <= root.Val && root.Val <= high {
+        acc = root.Val
+    }
+    
     if low <= root.Val {
-        acc += rangeSumBST(root.Left, low, high)
+        acc += rec(root.Left, low, high)
     }
     
     if root.Val <= high {
-        acc += rangeSumBST(root.Right, low, high)
+        acc += rec(root.Right, low, high)
     }
     
     return acc
 }
-
-
