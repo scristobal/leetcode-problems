15a607acf09d19bf6f365d9407cf85311bb32814
Samu <9478529+scristobal@users.noreply.github.com>
2022-10-08 17:50:41 +0200

    Time: 1 ms (64.07%), Space: 2 MB (74.46%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.rs
+++ 58-length-of-last-word/58-length-of-last-word.rs
@@ -1,6 +1,6 @@
 impl Solution {
     pub fn length_of_last_word(s: String) -> i32 {
-         s.split(" ")
+         s.split_whitespace()
             .filter(|s| !s.is_empty())
             .last()
             .map(|s| s.len() as i32)
