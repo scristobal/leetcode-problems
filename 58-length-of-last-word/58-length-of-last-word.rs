3d20c6a76db4ee1f2627cbe5ce8b8b946a091d26
Samu <9478529+scristobal@users.noreply.github.com>
2022-10-08 17:47:33 +0200

    Time: 0 ms (100.00%), Space: 2.2 MB (7.36%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+         s.split(" ")
+            .filter(|s| !s.is_empty())
+            .last()
+            .map(|s| s.len() as i32)
+            .unwrap_or(0)
+    }
+}
