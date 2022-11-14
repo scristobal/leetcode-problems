f210de20cdce6bbdf91ac6a2f45ac16a52abb463
Samu <9478529+scristobal@users.noreply.github.com>
2022-11-14 08:36:22 -0500

    Time: 127 ms (15.83%), Space: 45.9 MB (5.04%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.ts
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function zigzagLevelOrder(root: TreeNode | null): number[][] {
+    
+    if (root === null) return [];
+       
+    let isEvenLvl = true; // lvl starts at 1 for root, hence root is an even lvl
+    const res= [[root.val]];
+    let nextLvl = [root.left, root.right];
+    
+    while (nextLvl.length > 0 ) {
+        
+        isEvenLvl = !isEvenLvl;
+        const currentLvl = nextLvl;
+        nextLvl = [];
+        
+        const lvlRes = [];
+        
+        while (currentLvl.length > 0 ){
+            
+            let node = currentLvl.shift();
+        
+            if (node === null) continue
+
+            if (isEvenLvl) {
+                lvlRes.push(node.val)
+            } else {
+                lvlRes.unshift(node.val)
+            }
+
+            nextLvl.push(node.left, node.right)
+        }
+        
+        if (lvlRes.length > 0 ) { res.push(lvlRes)}
+        
+    }
+    
+    return res
+    
+};
