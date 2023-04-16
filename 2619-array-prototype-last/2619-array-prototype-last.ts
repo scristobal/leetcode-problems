22833e426478b2fe471d6a7499dfa82bac9ce395
Samu <9478529+scristobal@users.noreply.github.com>
2023-04-16 09:18:05 +0100

    Time: 57 ms (80.49%), Space: 43.3 MB (22.36%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.ts
@@ -0,0 +1,18 @@
+declare global {
+    interface Array<T> {
+        last(): T | -1;
+    }
+}
+
+Array.prototype.last = function() {
+    
+    return this.length === 0 ? 
+        -1:
+        this[this.length-1];
+
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
