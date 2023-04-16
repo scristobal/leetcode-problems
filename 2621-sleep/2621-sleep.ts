54f3cdb83848faa2353440e13f9b75acdd186749
Samu <9478529+scristobal@users.noreply.github.com>
2023-04-16 16:02:14 +0100

    Time: 64 ms (53.01%), Space: 42.7 MB (54.62%) - LeetHub

---
+++ 2621-sleep/2621-sleep.ts
@@ -0,0 +1,9 @@
+async function sleep(millis: number): Promise<void> {
+    return new Promise( (resolve, reject) => setTimeout(resolve, millis) );
+}
+
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
