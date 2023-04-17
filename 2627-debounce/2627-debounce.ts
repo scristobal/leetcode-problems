4a721b0c696d30ed02ddefccd3db6454330f74b3
Samu <9478529+scristobal@users.noreply.github.com>
2023-04-17 07:39:49 +0100

    Time: 57 ms (91.45%), Space: 42.6 MB (76.92%) - LeetHub

---
+++ 2627-debounce/2627-debounce.ts
@@ -0,0 +1,20 @@
+type F = (...p: any[]) => any
+
+function debounce(fn: F, t: number): F {
+    
+    let timeout;
+    
+    return function(...args) {
+        
+        clearTimeout(timeout);
+            
+        timeout = setTimeout(() => fn(...args), t );
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
