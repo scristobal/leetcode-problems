6d3802f1586a0f53ec2e39049b7d52737c42db11
Samu <9478529+scristobal@users.noreply.github.com>
2023-10-08 18:29:18 +0200

    Time: 58 ms (18.07%), Space: 42 MB (27.71%) - LeetHub

---
+++ 2776-convert-callback-based-function-to-promise-based-function/2776-convert-callback-based-function-to-promise-based-function.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ * @return {Function<Promise<number>>}
+ */
+var promisify = function(fn) {
+ 	return async function(...args) {
+        
+        let res
+        
+        const callback = (value, error) => {
+            if (error) throw (error)
+            console.log(value)
+            res = value
+        }
+        
+        fn(callback, ...args)
+        
+        return res
+	}
+};
+
+/**
+ * const asyncFunc = promisify(callback => callback(42));
+ * asyncFunc().then(console.log); // 42
+ */
