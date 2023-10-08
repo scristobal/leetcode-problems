9ced2d75b1b10fb4df29e39ae2f204854ca423e6
Samu <9478529+scristobal@users.noreply.github.com>
2023-10-08 17:45:00 +0200

    Time: 60 ms (75.79%), Space: 42.1 MB (55.79%) - LeetHub

--- 2795-parallel-execution-of-promises-for-individual-results-retrieval/2795-parallel-execution-of-promises-for-individual-results-retrieval.js
+++ 2795-parallel-execution-of-promises-for-individual-results-retrieval/2795-parallel-execution-of-promises-for-individual-results-retrieval.js
@@ -4,29 +4,26 @@
  */
 var promiseAllSettled = function(funcs) {
     
-    
     return new Promise((accept, reject) => {
         
-        const res = funcs.map(() => ({status: "unkown"}))
+        const res = new Array(funcs.length);
+        let completed = 0;
     
         for (const [i, func] of funcs.entries()) {
-            func().then(v => res[i] = { status: "fulfilled", value: v }).catch(e => res[i] = { status: "rejected", reason: e})
-        }
-    
-        
-        function wait() { 
-            setTimeout(() => {
-               
-                const unkowns = res.find(({status}) => status === "unkown")
-            
-                if (unkowns === undefined) accept(res)
-                else wait()
-            
-            }, 1)
-        }
-            
-            
-        wait()
+            func()
+                .then(v => {
+                    res[i] = { status: "fulfilled", value: v }
+                    completed++
+                })
+                .catch(e => {
+                    res[i] = { status: "rejected", reason: e}
+                    completed++
+                })
+                .finally(() => {
+                    if (completed === funcs.length) accept(res)
+                })
+        }     
+       
     })
 };
 
