d6c5d3f599eef2fb304841702b75766c60fc031a
Samu <9478529+scristobal@users.noreply.github.com>
2023-10-30 18:55:23 +0100

    Time: 64 ms (89.81%), Space: 44.6 MB (65.28%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
@@ -2,37 +2,15 @@
  * @param {number[]} arr
  * @return {number[]}
  */
-var sortByBits = function(arr) {
-  
-    
-    return arr.sort((a,b) => {
-        const bA = numBits(a);
-        const bB = numBits(b);
-        
-        const d = bA - bB;
-        
-        if (d!==0) return d
-        else return a-b
-    })
-};
+var sortByBits = (arr) => arr.sort((a,b) => nBits(a) - nBits(b) || a-b)
 
 
-function numBits(n){
-    if (n===0) return 0;
-    
-  
-    
-    const h = Math.floor(n/2);
-
-    if ( (n % 2) === 1) {
-        const b = numBits(h) + 1;
-      
-        return b
-    }
-    
-    const b = numBits(h);
-    
-    return b
-    
+function nBits(n){
+   let r = 0
+   while(n) {
+       r += n & 1
+       n = n >> 1
+   }
+   return r
 };
 
