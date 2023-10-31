78187ddff5f21a7744fa55fb958d40f7b93e0aa1
Samu <9478529+scristobal@users.noreply.github.com>
2023-10-31 09:30:20 +0100

    Time: 3 ms (25.00%), Space: 2.2 MB (50.00%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.rs
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.rs
@@ -1,20 +1,20 @@
 impl Solution {
     pub fn sort_by_bits(arr: Vec<i32>) -> Vec<i32> {
-    let mut a = arr.clone();
+        let mut a = arr.clone();
         
-    a.sort_by(|&a, &b| {
-        let ones_a = i32::count_ones(a);
-        let ones_b = i32::count_ones(b);
+        a.sort_by(|&a, &b| {
+            let ones_a = i32::count_ones(a);
+            let ones_b = i32::count_ones(b);
 
-        match ones_a.partial_cmp(&ones_b) {
-            Some(ord) => match ord {
-                std::cmp::Ordering::Less => std::cmp::Ordering::Less,
-                std::cmp::Ordering::Equal => a.partial_cmp(&b).unwrap(),
-                std::cmp::Ordering::Greater => std::cmp::Ordering::Greater,
-            },
-            None => unreachable!(),
-        }
-    });
-    a
-}
+            match ones_a.partial_cmp(&ones_b) {
+                Some(ord) => match ord {
+                    std::cmp::Ordering::Equal => a.partial_cmp(&b).unwrap(),
+                    _ => ord,
+                },
+                _ => unreachable!(),
+            }
+        });
+        
+        a
+    }
 }
