f3409586dbc524c4aeced37b9632ad453b461639
Samu <9478529+scristobal@users.noreply.github.com>
2023-11-01 08:24:05 +0100

    Time: 3 ms (70.00%), Space: 3.2 MB (30.00%) - LeetHub

---
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.rs
@@ -0,0 +1,56 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashMap;
+impl Solution {
+    pub fn find_mode(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut count = HashMap::new();
+    let mut stack = vec![];
+
+    stack.push(root);
+
+    while let Some(node) = stack.pop() {
+        match node {
+            Some(node) => {
+                let node = node.borrow();
+
+                count.entry(node.val).and_modify(|e| *e += 1).or_insert(1);
+                stack.push(node.left.clone());
+                stack.push(node.right.clone());
+            }
+            None => continue,
+        }
+    }
+
+    count
+        .into_iter()
+        .fold((vec![], 0), |(mut modes, max), (val, count)| {
+            match count.cmp(&max) {
+                std::cmp::Ordering::Greater => (vec![val], count),
+                std::cmp::Ordering::Equal => {
+                    modes.push(val);
+                    (modes, max)
+                }
+                _ => (modes, max),
+            }
+        })
+        .0
+}
+}
