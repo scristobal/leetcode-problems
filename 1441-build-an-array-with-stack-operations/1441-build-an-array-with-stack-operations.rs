7127fd864be3fabc7caa7c27830f4acb5851822b
Samu <9478529+scristobal@users.noreply.github.com>
2023-11-03 18:54:11 +0100

    Time: 1 ms (84.62%), Space: 2 MB (100.00%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.rs
@@ -0,0 +1,26 @@
+
+static PUSH: &str = "Push";
+static POP: &str = "Pop";
+
+impl Solution {
+ pub fn build_array(target: Vec<i32>, n: i32) -> Vec<String> {
+    let mut operations: Vec<String> = vec![];
+
+    let mut next: i32 = 1;
+
+    for i in target.into_iter() {
+        operations.push(PUSH.to_string());
+
+        for _ in next..i {
+            operations.push(POP.to_string());
+            operations.push(PUSH.to_string());
+        }
+
+        next = i + 1;
+    }
+
+    operations
+}
+}
+
+
