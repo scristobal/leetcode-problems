2dc0e8b5bc149b07d3353612a084f1afa2a6d90b
Samu <9478529+scristobal@users.noreply.github.com>
2023-11-03 18:59:02 +0100

    Time: 1 ms (84.62%), Space: 2.1 MB (76.92%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.rs
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.rs
@@ -3,26 +3,26 @@ static PUSH: &str = "Push";
 static POP: &str = "Pop";
 
 impl Solution {
- pub fn build_array(target: Vec<i32>, n: i32) -> Vec<String> {
-    let mut operations: Vec<String> = vec![];
-     
-    operations.reserve(target.len() * 2);
+    pub fn build_array(target: Vec<i32>, n: i32) -> Vec<String> {
+        let mut operations: Vec<String> = vec![];
 
-    let mut next: i32 = 1;
+        operations.reserve(target.len() * 2);
 
-    for i in target.into_iter() {
-        operations.push(PUSH.to_string());
+        let mut next: i32 = 1;
 
-        for _ in next..i {
-            operations.push(POP.to_string());
+        for i in target.into_iter() {
             operations.push(PUSH.to_string());
+
+            for _ in next..i {
+                operations.push(POP.to_string());
+                operations.push(PUSH.to_string());
+            }
+
+            next = i + 1;
         }
 
-        next = i + 1;
+        operations
     }
-
-    operations
-}
 }
 
 
