79816439839e2931d3581e6e660f83b42c55d4a4
Samu <9478529+scristobal@users.noreply.github.com>
2023-11-04 06:24:09 +0100

    Time: 3 ms (80.00%), Space: 2.4 MB (40.00%) - LeetHub

--- 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.rs
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.rs
@@ -1,12 +1,13 @@
 
 impl Solution {
     pub fn get_last_moment(n: i32, left: Vec<i32>, right: Vec<i32>) -> i32 {
-        
-        std::cmp::max(
-            *left.iter().max().or(Some(&0)).unwrap(), 
-            right.iter().map(|l| n-l ).max().or(Some(0)).unwrap()
-        )
-        
-       
+        left
+            .into_iter()
+            .chain(right
+                .into_iter()
+                .map(|l| n-l ))
+            .max()
+            .or(Some(0))
+            .unwrap()
     }
 }
